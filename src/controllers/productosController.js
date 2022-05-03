36bbf87b261442a306d0d0cf85557ff5432ecf53
mateocampillo <mateocampillo1@gmail.com>
2022-05-03 14:20:50 -0300

    Nueva funcion para agregar productos nuevos a la base de datos

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,17 +1,56 @@
+const req = require("express/lib/request");
 const fs = require("fs");
 let productosEnJSON = fs.readFileSync(__dirname + "/../models/Productos.json","utf-8");
 
 const controller = {
+
+        //Controlador para desplegar pregunto segun el id pasado en la ruta
+
     detalle: (req, res) => {
         let productos = JSON.parse(productosEnJSON);
         res.status(200).render("./products/ProductDetail", {productos: productos[req.params.id-1]});
     },
+
+        //Controladores para el carrito
+
     cart: (req, res) => {
         res.status(200).render("./products/ProductCart");
     },
+
+        //Controladores para la seccion de agregar productos
+
     add: (req, res) => {
-        res.status(200).render();
+        let arrProductos = JSON.parse(productosEnJSON);
+        let lengthArr = arrProductos.length
+        res.status(200).render("./products/ProductAdd", {idDeProximo: lengthArr+1});
     },
+    added: (req, res) => {
+        const body = req.body;
+        let prodAdded = {
+            id: body.numAddId,
+            name: body.txtAddName,
+            description: body.txtAddDesc,
+            category: body.radioAddCat,
+            price: body.numAddPrice,
+            type: body.radioAddType,
+            size: body.radioAddSize,
+            waist: body.txtAddWaist,
+            chest: body.txtAddChest,
+            back: body.txtAddBack,
+            height: body.txtAddHeight,
+            cloth: body.radioAddCloth,
+            color: body.radioAddColor,
+            code: body.txtAddCode
+        }
+        res.status(200).render("./products/ProductAdded");
+        let arrProductos = JSON.parse(productosEnJSON);
+        arrProductos.push(prodAdded);
+        let prodEnJson = JSON.stringify(arrProductos);
+        fs.writeFileSync(__dirname + "/../models/Productos.json", prodEnJson);
+    },
+
+        //Controladores para la seccion de edicion de productos
+
     edit: (req, res) => {
         let arrayProductos = JSON.parse(productosEnJSON);
         res.status(200).render("./products/ProductEdit", {productos: arrayProductos[req.params.id-1]});
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -7,6 +7,8 @@ router.get("/detalle/:id", productosController.detalle);
 router.get("/carrito", productosController.cart);
 router.get("/editar/:id", productosController.edit);
 router.post("/guardar", productosController.save);
+router.get("/agregar", productosController.add);
+router.post("/agregado", productosController.added);
 
 
 module.exports = router;
