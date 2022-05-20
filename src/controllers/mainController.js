c244fe32926c2e901a6e4f6bd0de0bb371988c57
mateocampillo <mateocampillo1@gmail.com>
2022-05-20 20:02:10 -0300

    agregue la vista de admin, falta el borrar productos para agregar esa funcionalidad

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 
 let productosEnJSON = fs.readFileSync(__dirname + "/../data/Productos.json","utf-8");
 let productos = JSON.parse(productosEnJSON);
+let usersEnJSON = fs.readFileSync(__dirname + "/../data/Users.json","utf-8");
+let usersArray = JSON.parse(usersEnJSON);
+
 
 const controller = {
 
@@ -9,6 +12,10 @@ const controller = {
 
     index: (req, res) => {
         res.status(200).render("./main/index", {productos: productos});
+    },
+
+    admin: (req, res) => {
+        res.status(200).render("./main/admin", {productos: productos, users: usersArray});
     }
 }
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mainController = require("../controllers/mainController");
 
 router.get("/", mainController.index);      //Entry point al localhost:3000
+router.get("/admin", mainController.admin);         //Vista de admin
 
 module.exports = router;
