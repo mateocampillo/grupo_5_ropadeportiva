e8ae33a6b13803341edba82d7494b34447b23aee
mateocampillo <mateocampillo1@gmail.com>
2022-07-20 14:47:32 -0300

    agrega completa funcionalidad de sacar productos del carrito y modifique el input de cantidades de items; borre el css de ProductCartPrueba

--- public/js/listarCarrito.js
+++ public/js/listarCarrito.js
@@ -1,6 +1,10 @@
 window.addEventListener('load', function() {
 
     let listaCarrito = document.querySelector('#lista-productos-carrito');
+    let h2Envio = document.querySelector('.h2-envio');
+    let hr = document.querySelector('.hr-1');
+    let containerBotones = document.querySelector('.container-botones');
+    let containerPrecioTotal = document.querySelector('.container-botones');
     let productosEnCarrito = JSON.stringify(localStorage.getItem('CarritoProductos'));
     let num = productosEnCarrito.match(/\d+/g);
     let totalCompra = document.querySelector('#total-compra');
@@ -18,41 +22,90 @@ window.addEventListener('load', function() {
 
     let displayProducts = function(products) {
 
-        let carritoFiltrado = [];
-        products.forEach( item => {
-            num.forEach( number => {
-                if(number == item.id){
-                    carritoFiltrado.push(item);
-                }
-            })
-        });
+        //Seccion listar el carrito
 
-        let htmlString = carritoFiltrado.map( product => {
-            return `
-            <li class="container-li">
-                    <div class="container-producto">
-                        <div class="container-imagen"><img class="img-producto" src="/images/ProductDetail/${product.img1}" alt="foto producto"></div>
-                        <div class="nombre-producto"><h2 >${product.name}</h2></div>
-                        <div class="precio-producto"><p>$${product.price}</p></div>
-                        <div class="container-botones">
-                            <h3 class="cantidad-titulo">Cantidad:</h3>
-                            <input class="input-cantidad" value="1" min="1" max="5"></input>
+        if(!localStorage.getItem('CarritoProductos')){
+
+            h2Envio.innerHTML = `<h2>Parece que todavia no tiene objetos en el carrito!</h2>`;
+            hr.style.display = 'none';
+            listaCarrito.style.display = 'none';
+            containerBotones.style.display = 'none';
+            listaCarrito.style.display = 'none';
+            containerPrecioTotal.style.display = 'none';
+
+        } else {
+
+            let carritoFiltrado = [];
+            products.forEach( item => {
+                num.forEach( number => {
+                    if(number == item.id){
+                        carritoFiltrado.push(item);
+                    }
+                })
+            });
+    
+            let htmlString = carritoFiltrado.map( product => {
+                return `
+                <li class="container-li">
+                        <div class="container-producto">
+                            <div><input type="hidden" class="nombre-producto-input" value="${product.name}"></div>
+                            <div class="container-imagen"><img class="img-producto" src="/images/ProductDetail/${product.img1}" alt="foto producto"></div>
+                            <div class="nombre-producto"><h2>${product.name}</h2></div>
+                            <div class="precio-producto"><p>$${product.price}</p></div>
+                            <div class="container-botones">
+                                <h3 class="cantidad-titulo">Cantidad:</h3>
+                                <input class="input-cantidad" value="1" readonly></input>
+                            </div>
+                            <div class="container-botones"><button class="eliminar-producto">Eliminar</button></div>
                         </div>
-                        <div class="container-botones"><button class="eliminar-producto">Eliminar</button></div>
-                    </div>
-                </li>
-            `;
-        })
-        .join('');
-
-        let contadorPrecioTotal = 0;
-        carritoFiltrado.forEach( producto => {
-            contadorPrecioTotal = contadorPrecioTotal + producto.price;
-        })
-        totalCompra.innerHTML = `Total a abonar: $${contadorPrecioTotal}`;
-
-        listaCarrito.innerHTML = htmlString;
-    };
+                    </li>
+                `;
+            })
+            .join('');
+    
+            listaCarrito.innerHTML = htmlString;
+    
+            let contadorPrecioTotal = 0;
+            carritoFiltrado.forEach( producto => {
+                contadorPrecioTotal = contadorPrecioTotal + producto.price;
+            })
+            totalCompra.innerHTML = `Total a abonar: $${contadorPrecioTotal}`;
+    
+            //Seccion eliminar productos del carrito
+    
+            let botonesEliminar = document.querySelectorAll('.eliminar-producto');
+            botonesEliminar.forEach( boton => {
+    
+                boton.addEventListener('click', function(event) {
+    
+                    let nameProductDelete = event.target.parentElement.parentElement.firstChild.nextSibling.firstChild.attributes.value.textContent;
+                    let arrCarrito = JSON.parse(localStorage.getItem('CarritoProductos'));
+    
+                    carritoFiltrado.forEach(product => {
+                        if(product.name == nameProductDelete){
+                            let idProductDelete = product.id;
+    
+                            let index = arrCarrito.indexOf(idProductDelete.toString());
+                            if(index > -1){
+                                arrCarrito.splice(index, 1);
+                            }
+    
+                            if(arrCarrito.length < 1){
+                                localStorage.removeItem('CarritoProductos');
+                                location.reload();
+                            } else if (arrCarrito.length >= 1){
+                                localStorage.setItem('CarritoProductos', JSON.stringify(arrCarrito));
+                                location.reload();
+                            }
+                        
+                        }
+                    })
+    
+                });
+           })
+    
+        };
+        }
 
     loadProducts();
 
