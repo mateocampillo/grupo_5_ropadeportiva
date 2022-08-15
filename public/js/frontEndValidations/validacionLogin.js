785c32f9f81200f4a9c51edc0c2cad09e1000b33
mateocampillo <mateocampillo1@gmail.com>
2022-08-15 14:07:57 -0300

    fix validacion front para el login

--- public/js/frontEndValidations/validacionLogin.js
+++ public/js/frontEndValidations/validacionLogin.js
@@ -2,13 +2,15 @@ window.addEventListener('load', function() {
 
     let email = document.querySelector('#emailUserEmail');
     let contraseña = document.querySelector('#txtPassword');
-    let inputErrEmail = document.querySelector('#InputErrorEmailUserEmail');
+    let inputErrEmail = document.querySelector('#inputErrorEmailUserEmail');
     let inputErrTxtPassword = document.querySelector('#inputErrorTxtPassword');
         
         email.addEventListener('input', function() {
 
             if(email.value.length == 0){
                 inputErrEmail.innerHTML = 'El email no puede estar vacio'
+            } else if(!email.value.includes('@')) {
+                inputErrEmail.innerHTML = "Debes introducir un email valido"
             } else {
                 inputErrEmail.innerHTML = ''
             }
